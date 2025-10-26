d4b7d7bec53c5f6c053e111804aea725dc92065e
Flaze07 <christianseiji.cs@gmail.com>
2025-10-27 01:12:12 +0700

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+trait Pet {
+    fn talk(&self) -> String;
+
+    fn greet(&self) {
+        println!("Oh you're a cuitie! What's your name? {}", self.talk());
+    }
+}
+
+struct Dog {
+    name: String,
+    age: i8,
+}
+
+impl Pet for Dog {
+    fn talk(&self) -> String {
+        format!("Woof, my name is {}", self.name)
+    }
+}
+
 fn main() {
-    println!("Hello new main")
+    let fido = Dog { name: String::from("Fido"), age: 5 };
+    dbg!(fido.talk());
+    fido.greet();
 }

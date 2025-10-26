06f832ea5b9e4cd6a4e3af45f743ac221af5f049
Flaze07 <flaze07@github.com>
2025-10-26 21:05:01 +0700

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+enum Result {
+    Ok(i32),
+    Err(String),
+}
+
+fn divide_in_two(n: i32) -> Result {
+    if n % 2 == 0 {
+        Result::Ok(n / 2)
+    } else {
+        Result::Err(format!("Cannot divide {n} into two equal parts"))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 100;
+    match divide_in_two(n) {
+        Result::Ok(half) =>
+            println!("{n} divided in two is {half}"),
+        Result::Err(msg) =>
+            println!("Sorry, an error happened: {msg}"),
+    }
 }

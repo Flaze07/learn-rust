563d756b5304e9f1068311a764fb8f417dddd441
Flaze07 <christianseiji.cs@gmail.com>
2025-10-27 22:45:58 +0700

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("EVEN NEWER MAIN");
+    let max_value = 5;
+    let clamp = |v| {
+        if v > max_value { max_value } else { v }
+    };
+
+    dbg!(clamp(1));
+    dbg!(clamp(3));
+    dbg!(clamp(5));
+    dbg!(clamp(7));
+    dbg!(clamp(10));
 }

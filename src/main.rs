f10add42bec89875361438f63432ebc3f8257bc0
Flaze07 <christianseiji.cs@gmail.com>
2025-10-27 22:44:49 +0700

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("EVEN NEWER MAIN");
+    let double_it = |n| n + 2;
+    dbg!(double_it(50));
+
+    let add_1f32 = |x: f32| -> f32 { x + 1.0 };
+    dbg!(add_1f32(50.));
 }

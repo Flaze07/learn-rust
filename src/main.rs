fc99f700e0e9d8d935196db8c8c9b71f62fae0c1
Flaze07 <christianseiji.cs@gmail.com>
2025-10-27 22:22:26 +0700

    generic-min

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::cmp::Ordering;
+
+fn min<T: Ord>(a: T, b: T) -> T {
+    if a < b { a } else { b }
+}
+
+#[test]
+fn integers() {
+    assert_eq!(min(0, 10), 0);
+    assert_eq!(min(500, 123), 123);
+}
+
+#[test]
+fn strings() {
+    assert_eq!(min("hello", "goodbye"), "goodbye");
+    assert_eq!(min("bat", "armadillo"), "armadillo");
+}
+
 fn main() {
-    println!("Hello new main")
+    // println!("Hello new main")
 }

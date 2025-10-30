5d95ef811d22109e60caab719b893209e36199ee
Flaze07 <christianseiji.cs@gmail.com>
2025-10-30 18:29:56 +0700

    reset main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
-    let name = "我们不一样";
-    let mut position: Option<usize> = name.find("我");
-    dbg!(position);
-    assert_eq!(position.unwrap(), 0);
-    position = name.find('Z');
-    dbg!(position);
-    assert_eq!(position.is_none(), true);
+    println!("HELLO WORLD AND HELLO THE END");
 }

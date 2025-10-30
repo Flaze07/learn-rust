04fe8c49f80fb37219c2b738c1bfada393778a55
Flaze07 <christianseiji.cs@gmail.com>
2025-10-30 18:28:58 +0700

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("EVEN NEWER MAIN");
+    let name = "我们不一样";
+    let mut position: Option<usize> = name.find("我");
+    dbg!(position);
+    assert_eq!(position.unwrap(), 0);
+    position = name.find('Z');
+    dbg!(position);
+    assert_eq!(position.is_none(), true);
 }

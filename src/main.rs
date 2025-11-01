fdb897feb5004707595e0502370d6fc27b4a2842
Flaze07 <christianseiji.cs@gmail.com>
2025-11-01 09:14:04 +0700

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("HELLO WORLD AND HELLO THE END");
+    let mut page_counts = HashMap::new();
+    page_counts.insert("Adventures of Huckleberry Finn", 207);
+    page_counts.insert("Grimms' Fairy Tales", 751);
+    page_counts.insert("Pride and Prejudice", 303);
+
+    if !page_counts.contains_key("Les Misérables") {
+        println!(
+            "We know about {} books but not Les Misérables.",
+            page_counts.len()
+        )
+    }
+
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        match page_counts.get(book) {
+            Some(count) => println!("{book}: {count} pages"),
+            None => println!("{book} is unknown."),
+        }
+    }
+
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        let page_count = page_counts.entry(book).or_insert(0);
+        *page_count += 1;
+    }
+
+    dbg!(page_counts);
 }
